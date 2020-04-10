e804c85cca006313c397bbd743c2a0c8aaed9672
dev-ng <nickolay.golomysov@gmail.com>
2020-04-10 16:25:05 +0200

    Task2 done

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -1,9 +1,34 @@
 package subtask2
 
 class TimeConverter {
+    val numbers = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+        "twenty", "twenty one", "twenty two", "twenty three", "twenty four",
+        "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine")
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        var hr = hour.toInt()
+        var mn = minute.toInt()
+        if( hr < 0 || hr > 12 || mn < 0 || mn > 59) return ""
+        var relation = ""
+        when (mn) {
+            0 -> return numbers[hr] + " o' clock"
+            15 -> return "quarter past " + numbers[hr]
+            30 -> return "half past " + numbers[hr]
+            45 -> return "quarter to " + numbers[hr+1]
+            else -> {
+                if (mn < 30) {
+                    relation = " past "
+                }else if (mn < 60) {
+                    relation = " to "
+                    mn = 60 - mn
+                    hr = hr % 12 + 1
+                }
+            }
+        }
+        if (1 == mn) {
+            return numbers[mn] + " minute" + relation + numbers[hr]
+        }
+        return numbers[mn] + " minutes" + relation + numbers[hr]
     }
 }
