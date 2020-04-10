9311fe14a38c2175b8bd39c99a5bcd6cf21768ac
dev-ng <nickolay.golomysov@gmail.com>
2020-04-10 23:49:53 +0200

    Task6 done

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,14 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        val seq = generateSequence(Pair(0, 1)) {
+            if (it.first * it.second >= n)
+                null
+            else
+                Pair(it.second, it.first + it.second)
+        }
+        val (min, max) = seq.last()
+        return intArrayOf(min, max, if (n == min * max) 1 else 0)
     }
 }
