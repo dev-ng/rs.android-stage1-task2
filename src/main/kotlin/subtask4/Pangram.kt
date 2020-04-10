839f6ccca0a0dba08128ed8ab2af20f7d25db8e4
dev-ng <nickolay.golomysov@gmail.com>
2020-04-10 22:45:48 +0200

    Task4 done

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,47 @@
 package subtask4
 
 class Pangram {
+    private val vowels = listOf<Char>( 'a', 'e', 'i', 'o', 'u', 'y' )
+    private val consonants = ('a'..'z').filterNot { it in vowels }
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        if (isPangram(inputString)) {
+            return fromPangram(inputString, vowels)
+        }
+        return fromNonPangram(inputString, consonants)
+    }
+
+    private fun isPangram(s: String): Boolean {
+        val lowercase = s.toLowerCase()
+        return ('a'..'z').all { lowercase.contains(it) }
+    }
+
+    private fun fromPangram(inputString: String, vocab: List<Char>): String {
+        var words: List<String> = inputString.split(" ")
+        words = words.filterNot { "" == it }
+        val pairs = mutableListOf<Pair<Int, String>>()
+        words.forEach {word: String ->
+            var newWord = ""
+            var i = 0
+            word.forEach { char: Char ->
+                if (char.toLowerCase() in vocab) {
+                    i += 1
+                    newWord = newWord + char.toUpperCase()
+                } else {
+                    newWord = newWord + char
+                }
+            }
+            pairs.add(pairs.filter { it.first <= i }.size, Pair(i, i.toString() + newWord))
+        }
+        val result = mutableListOf<String>()
+        pairs.forEach {pair ->
+            result.add(pair.second)
+        }
+        return result.joinToString(" ")
+    }
+
+    private fun fromNonPangram(inputString: String, vocab: List<Char>): String {
+        return fromPangram(inputString.replace("\n", " ").replace("\r", " "),
+            vocab)
     }
 }
